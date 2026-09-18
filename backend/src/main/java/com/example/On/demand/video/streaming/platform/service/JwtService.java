a59558aa9bc6762317952680c8e55e207cc85a88
varshinisri14 <varshinisrika@gmail.com>
2026-09-18 21:28:33 +0530

    feat(auth): implement JWT token service

---
+++ backend/src/main/java/com/example/On/demand/video/streaming/platform/service/JwtService.java
@@ -0,0 +1,49 @@
+package com.example.On.demand.video.streaming.platform.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final SecretKey secretKey =
+            Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(String email, String role) {
+
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+
+        return getClaims(token)
+                .getSubject();
+    }
+
+    public String extractRole(String token) {
+
+        return getClaims(token)
+                .get("role", String.class);
+    }
+
+    private Claims getClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
