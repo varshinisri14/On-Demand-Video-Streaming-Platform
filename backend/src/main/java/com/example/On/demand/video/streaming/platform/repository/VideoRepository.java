8be62402533459d5db0efec0fcb911ca2aabace1
varshinisri14 <varshinisrika@gmail.com>
2026-09-19 18:50:45 +0530

    feat(video): add video repository

---
+++ backend/src/main/java/com/example/On/demand/video/streaming/platform/repository/VideoRepository.java
@@ -0,0 +1,13 @@
+package com.example.On.demand.video.streaming.platform.repository;
+
+import com.example.On.demand.video.streaming.platform.model.entity.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface VideoRepository extends JpaRepository<Video, Integer> {
+
+    List<Video> findByEntrepreneurEntrepreneurId(Integer entrepreneurId);
+
+    List<Video> findByApprovalStatus(String approvalStatus);
+}
